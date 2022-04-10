38388380bbdd2090a2b7557fd4d5e87616533d23
Tergel Enkhtur <tergel.e9@gmail.com>
2022-04-09 19:28:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    //console.log(req.method)
+    console.log('Accept: ' + req.get('Accept'))
+    
+    res.send('<h1>Hello World!</h1>')
+
+    console.log('Content-Type: ' + res.get('Content-Type'))
 })
 
 app.listen(port, () => {
