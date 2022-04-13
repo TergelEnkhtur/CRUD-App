424b5048649382d5600ee121e59282464a06bff6
watermelonTea <78840753+watermelonTea@users.noreply.github.com>
2022-04-13 12:08:17 -0500

    changed port back to 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 28649;
+const port = process.env.PORT || 3000;
 
 // now updated with sendFile
 app.get('/', function(req, res) {
