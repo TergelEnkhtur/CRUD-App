75b300d703f89b376cbf00d3f3a563c3f407a84f
Zaeem Khan <60846184+zaeem-khan@users.noreply.github.com>
2022-04-14 16:08:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
 // now updated with sendFile
 app.get('/', (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
