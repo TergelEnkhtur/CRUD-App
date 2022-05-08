ee82e8c3577e728fe47c935533d2da1878f73ca4
Hinal <hpate154@depaul.edu>
2022-05-08 17:59:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./server');
 
 const port = process.env.PORT || 3000
-const path = require('path');
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
